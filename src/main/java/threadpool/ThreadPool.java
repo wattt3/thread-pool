3ee99d2d046a4bd898d9afd78fa6a72ef11cf662
wattt3 <chleodnr3@gmail.com>
2024-03-04 16:41:15 +0900

    shutdown tombstone 로직 execute시 shutdown Double Check

--- src/main/java/threadpool/ThreadPool.java
+++ src/main/java/threadpool/ThreadPool.java
@@ -3,42 +3,41 @@ package threadpool;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ThreadPool implements Executor {
+
+    private static final Runnable SHUTDOWN_TASK = () -> {
+    };
 
     private final BlockingQueue<Runnable> queue = new LinkedTransferQueue<>();
     private final Thread[] threads;
     private final AtomicBoolean started = new AtomicBoolean();
 
-    private boolean shutdown;
+    private final AtomicBoolean shutdown = new AtomicBoolean();
 
     public ThreadPool(int numThreads) {
         threads = new Thread[numThreads];
         for (int i = 0; i < numThreads; i++) {
             threads[i] = new Thread(() -> {
-                    while (!shutdown) {
-                        // 사용자가 임의적으로 InterruptedException 던지는 것 방지
-                        Runnable task = null;
-                        try {
-                            task = queue.take();
-                        } catch (InterruptedException e) {
+                for (; ; ) {
+                    try {
+                        final Runnable task = queue.take();
+                        if (task == SHUTDOWN_TASK) {
+                            break;
+                        } else {
+                            task.run();
                         }
-                        if (task != null) {
-                            try {
-                                task.run();
-                            } catch (Throwable t) {
-                                if (!(t instanceof InterruptedException)) {
-                                    System.err.println("Unexpected exception: ");
-                                    t.printStackTrace();
-                                }
-                            }
+                    } catch (Throwable t) {
+                        if (!(t instanceof InterruptedException)) {
+                            System.err.println("Unexpected exception: ");
+                            t.printStackTrace();
                         }
                     }
+                }
+                System.err.println("Shutting thread `" + Thread.currentThread().getName() + "`");
             });
-            // constructor 안에서 start 했을 때 queue를 참조하고 있으니까 호출이 완료될 때 까지, 그 전에 어느 시점에서 queue가 초기화될지 모호하다
-            // 시작하자마자 queue를 참조하려고 하는데, 아직 constructor(초기화)가 끝나지 않았을 수 있다.
-            // threads[i].start(); => lazy하게 변경
         }
     }
 
@@ -49,24 +48,34 @@ public class ThreadPool implements Executor {
                 thread.start();
             }
         }
+
+        if (shutdown.get()) {
+            throw new RejectedExecutionException();
+        }
+
         queue.add(command);
+
+        if (shutdown.get()) {
+            queue.remove(command);
+            throw new RejectedExecutionException();
+        }
     }
 
     public void shutdown() {
-        shutdown = true;
+        if (shutdown.compareAndSet(false, true)) {
+            for (int i = 0; i < threads.length; i++) {
+                queue.add(SHUTDOWN_TASK);
+            }
+        }
+
         for (Thread thread : threads) {
-            while (thread.isAlive()) {
-                // interrupt 이후 사용자가 InterruptedException을 잡아서 아무런 행동하지 않을 수 있음.
-                // => shutdown flag를 사용하는 이유
-                thread.interrupt();
+            do {
                 try {
-                    // isAlive할 때 무한 루프를 타면서 interrupt를 하기 떄문에 성능이 좋지 않다.
-                    // wait for this thread to die.
                     thread.join();
                 } catch (InterruptedException e) {
                     // Do not propagate to prevent incomplete shutdown.
                 }
-            }
+            } while (thread.isAlive());
         }
     }
 }
--- src/test/java/threadpool/ThreadPoolTest.java
+++ src/test/java/threadpool/ThreadPoolTest.java
@@ -1,31 +1,35 @@
 package threadpool;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
 import org.junit.jupiter.api.Test;
 
 class ThreadPoolTest {
 
     @Test
     void submittedTasksAreExecuted() throws InterruptedException {
-        final Executor executor = new ThreadPool(2);
+        final ThreadPool executor = new ThreadPool(2);
         final int numTasks = 100;
         final CountDownLatch latch = new CountDownLatch(numTasks);
 
-        for (int i = 0; i < numTasks; i++) {
-            final int finalI = i;
-            executor.execute(() -> {
-                System.err.println("Thread `" + Thread.currentThread().getName() + "` executes task " + finalI);
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                latch.countDown();
-            });
-        }
+        try {
+            for (int i = 0; i < numTasks; i++) {
+                final int finalI = i;
+                executor.execute(() -> {
+                    System.err.println(
+                        "Thread `" + Thread.currentThread().getName() + "` executes task "
+                            + finalI);
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    latch.countDown();
+                });
+            }
 
-        latch.await();
+        } finally {
+            executor.shutdown();
+        }
     }
 
 }
